82456f779ff9d5942cf74be637fa693e4b1b7abd
Joe Horne <joeh19@gmail.com>
2021-12-01 15:20:10 -0500

    Added .scalafmt.conf

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,10 +1,11 @@
 @main
 def day1 =
-  val depths = getInput(1).map(_.toInt)
-  val numIncreased = findNumIncreased(depths)
-  assert(numIncreased == 1583)
-  val windowedDepths = depths.sliding(3).map(_.sum).toVector
+  val depths               = getInput(1).map(_.toInt)
+  val numIncreased         = findNumIncreased(depths)
+  val windowedDepths       = depths.sliding(3).map(_.sum).toVector
   val windowedNumIncreased = findNumIncreased(windowedDepths)
+
+  assert(numIncreased == 1583)
   assert(windowedNumIncreased == 1627)
 
 def findNumIncreased(depths: Vector[Int]): Int =
