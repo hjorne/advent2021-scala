9ed1b436c7cbf7be02f5fa81b19e5b559cf39b5b
Joe Horne <joseph.e.horne@gmail.com>
2021-12-01 00:13:23 -0500

    Day 1

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,4 +1,11 @@
-import scala.io.Source
-
 @main
-def day1 = println("Hello world!")
+def day1 =
+  val depths = getInput(1).map(_.toInt)
+  val numIncreased = findNumIncreased(depths)
+  assert(numIncreased == 1583)
+  val windowedDepths = depths.sliding(3).map(_.sum).toVector
+  val windowedNumIncreased = findNumIncreased(windowedDepths)
+  assert(windowedNumIncreased == 1627)
+
+def findNumIncreased(depths: Vector[Int]): Int =
+  depths.zip(depths.tail).count { case (first, second) ⇒ second > first }
