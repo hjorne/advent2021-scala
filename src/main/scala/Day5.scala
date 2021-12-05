60ae23fa9691b344d76c1a69c3a24b75d71f2371
Joe Horne <joeh19@gmail.com>
2021-12-05 10:36:03 -0500

    Day 5

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -2,6 +2,16 @@ case class Point(x: Int, y: Int)
 case class Line(r1: Point, r2: Point):
   def isStraight: Boolean = r1.x == r2.x || r1.y == r2.y
 
+  def enumerate: Seq[Point] =
+    val Line(Point(x1, y1), Point(x2, y2)) = this
+    val dx                                 = (x2 - x1).sign
+    val dy                                 = (y2 - y1).sign
+    val xs = LazyList.iterate(x1)(_ + dx).takeWhile(_ != x2 + dx).toVector
+    val ys = LazyList.iterate(y1)(_ + dy).takeWhile(_ != y2 + dy).toVector
+    if xs.isEmpty then ys.map(Point(x1, _))
+    else if ys.isEmpty then xs.map(Point(_, y1))
+    else xs.zip(ys).map { case (x, y) ⇒ Point(x, y) }
+
 @main
 def day5 =
   val lines              = parse5
@@ -12,21 +22,11 @@ def day5 =
 
 def countOverlapping(lines: Seq[Line]): Int =
   lines
-    .flatMap(enumerate)
+    .flatMap(_.enumerate)
     .groupBy(identity)
     .values
     .map(_.length)
     .count(_ >= 2)
-
-def enumerate(line: Line): Seq[Point] =
-  val Line(Point(x1, y1), Point(x2, y2)) = line
-  val dx                                 = (x2 - x1).sign
-  val dy                                 = (y2 - y1).sign
-  val xs = LazyList.iterate(x1)(_ + dx).takeWhile(_ != x2 + dx).toVector
-  val ys = LazyList.iterate(y1)(_ + dy).takeWhile(_ != y2 + dy).toVector
-  if xs.isEmpty then ys.map(Point(x1, _))
-  else if ys.isEmpty then xs.map(Point(_, y1))
-  else xs.zip(ys).map { case (x, y) ⇒ Point(x, y) }
 
 def parse5: Seq[Line] =
   val line = raw"(\d+),(\d+) -> (\d+),(\d+)".r
